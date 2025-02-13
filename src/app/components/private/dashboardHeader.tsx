e80e8a2589a923df15366d57cac2da872d54f805
Matheussmaced <mateusbyakuya3@gmail.com>
2025-02-13 20:17:22 -0300

    feat: creating dashboard header

--- src/app/components/private/dashboardHeader.tsx
+++ src/app/components/private/dashboardHeader.tsx
@@ -1,7 +1,27 @@
+import { ChartLine, LinkIcon, Settings, Wallpaper } from "lucide-react";
+import Link from "next/link";
+
 export default function DashboardHeader() {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="text-gray-800 p-6 shadow-md">
+      <div className="ml-12 w-screen flex gap-14">
+        <Link href='/dashboard/links' className="flex items-center gap-3 hover:text-blueLink transition-colors delay-75">
+          <LinkIcon />
+          <span>Links</span>
+        </Link>
+        <Link href='/dashboard/appearance' className="flex items-center gap-3 hover:text-blueLink transition-colors delay-75">
+          <Wallpaper />
+          <span>Aparência</span>
+        </Link>
+        <Link href='/dashboard/analytics' className="flex items-center gap-3 hover:text-blueLink transition-colors delay-75">
+          <ChartLine />
+          <span>análise</span>
+        </Link>
+        <Link href='/dashboard/settings' className="flex items-center gap-3 hover:text-blueLink transition-colors delay-75">
+          <Settings />
+          <span>Configurações</span>
+        </Link>
+      </div>
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,12 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        blueLink: '#6988D5'
+      }
+    }
+  },
   plugins: [],
 } satisfies Config;
